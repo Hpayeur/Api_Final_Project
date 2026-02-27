b76362f9b8b57cb2add3cb9a0ac5d9e9ed535dfa
Hpayeur <hailpaye017@gmail.com>
2026-02-26 17:29:37 -0700

    task/goals

---
+++ server.js
@@ -0,0 +1,7 @@
+//Goals and Tasks for the project
+
+// A simple server hosted on port 3000 or any other server of your choice
+// Folder structure for your routes, controllers, models and servers
+// At least one route for the upcoming project that will only include the text output of "Hello from my route" or something of similar layout. That means that you should be able to show one route working with the node modules you need
+// There should be no node_modules uploaded alongside the project, you should be able to git clone the project and run npm install to install the required modules to show the single route
+// You should have at least started your frontend portion as well.
